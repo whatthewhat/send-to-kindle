16ed5d4af7a217a0dad65355a6e1089ae3d19f67
whatthewhat <mikhail.topolskiy@gmail.com>
2011-03-11 18:10:12 +0300

    Moved methods to helpers

--- sendtokindle.rb
+++ sendtokindle.rb
@@ -2,32 +2,35 @@ require 'sinatra'
 require 'pony'
 require 'open-uri'
 
-def get_name(url)
-	name = url.scan(/[^\/]*$/)[0]
-	if name=~/\./
-		name
-	else
-		name + '.pdf'
+helpers do
+	# Provides a file name from the file url
+	def get_name(url)
+		name = url.scan(/[^\/]*$/)[0]
+		if name=~/\./
+			name
+		else
+			name + '.pdf'
+		end
 	end
-end
-
-def send_email(url, email)
-	open(url) do |f|
-		filename = get_name(url)
-
-		Pony.mail(:to => email, 
-		:from => 'Send To Kindle',
-		:subject => 'Convert',
-		:attachments => {filename => f.read},
-		:via => :smtp, :via_options => {
-			:address              => 'smtp.gmail.com',
-			:port                 => '587',
-			:enable_starttls_auto => true,
-			:user_name            => 'example@example.com',
-			:password             => 'password',
-			:authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-			:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
-		})
+	
+	# Downloads the file and sends it to the appropriate address
+	def send_email(url, email)
+		open(url) do |f|
+			filename = get_name(url)
+			Pony.mail(:to => email, 
+			:from => 'Send To Kindle',
+			:subject => 'Convert',
+			:attachments => {filename => f.read},
+			:via => :smtp, :via_options => {
+				:address              => 'smtp.gmail.com',
+				:port                 => '587',
+				:enable_starttls_auto => true,
+				:user_name            => 'example@example.com',
+				:password             => 'password',
+				:authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+				:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+			})
+		end
 	end
 end
 
@@ -39,9 +42,7 @@ end
 get '/send' do
 	@url = params[:url]
 	@email = params[:email]
-
 	@email = @email.chomp + '@free.kindle.com' unless @email=~/@/
-	
 	send_email(@url, @email)
 
 	erb :send
